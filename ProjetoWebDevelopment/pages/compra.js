de649b7be1239d39e083a2d39b7667b68cf35b65
Pereira <p.leonardo7k@gmail.com>
2024-11-21 19:51:40 -0300

    bruno fernandex

--- ProjetoWebDevelopment/pages/compra.js
+++ ProjetoWebDevelopment/pages/compra.js
@@ -1,15 +1,19 @@
 // Função para extrair parâmetros da URL
 function getQueryParameter(param) {
-    const urlParams = new URLSearchParams(window.location.search);
-    return urlParams.get(param);
-  }
-  
-  // Exibe o nome do produto
-  const productName = getQueryParameter('produto');
-  if (productName) {
-    document.getElementById('product-name').textContent = decodeURIComponent(productName);
-  }
-  
-  <style>
-    
-  </style>
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+// Exibe o nome do produto
+const productName = getQueryParameter('produto');
+if (productName) {
+  document.getElementById('product-name').textContent = decodeURIComponent(productName);
+}
+
+// Evento de submissão do formulário para redirecionar para a tela de agradecimento
+document.getElementById('purchaseForm').addEventListener('submit', function (event) {
+  event.preventDefault(); // Previne o envio padrão do formulário
+
+  // Redireciona para a página de agradecimento
+  window.location.href = 'agradecimento.html';
+});
